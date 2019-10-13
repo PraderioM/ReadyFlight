1b9b4f191cc3b184077259109dc8a9d783c5e5c9
PraderioM <PraderioM@Gmail.com>
2019-10-13 06:17:25 +0200

    Corrected bug in setting of maximum score.

--- backend/scores/update_max_score.py
+++ backend/scores/update_max_score.py
@@ -4,19 +4,22 @@ from aiohttp import web
 async def update_max_score(request):
     token = request.rel_url.query['token']
     score = request.rel_url.query['score']
-    print(request)
-    print(score)
     if score == '':
         score = 0
     else:
         score = float(score)
 
-    print(score)
-
     async with request.app['db'].acquire() as db:
+        existing_score = await db.fetchval("""
+                                          SELECT single_player_score
+                                          FROM users
+                                          WHERE token = $1
+                                          """, token)
+        score = max(score, existing_score)
+
         await db.execute("""
                          UPDATE users
-                         SET single_player_score = max(single_player_score, $1)
+                         SET single_player_score = $1
                          WHERE token = $2
                          """, score, token)
 
