d8a582160c85ae16293cee7fb2e4689c827110f7
PraderioM <PraderioM@Gmail.com>
2020-04-16 22:42:24 +0200

    Reformatted.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {NgModule, OnDestroy, OnInit} from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {RegisteringComponent} from './registering/registering.component';
